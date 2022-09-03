e6e1adb3a0530d20bc67d25b69fa06c4d834547b
bytedevrepo <npalien4u@gmail.com>
2022-09-03 16:48:54 +0530

    - install routes fixed

--- Modules/Installer/Routes/web.php
+++ Modules/Installer/Routes/web.php
@@ -14,18 +14,8 @@
 
 Route::group(['middleware' => ['IsNotInstalledCheck', 'XSS']], function () {
 
-    Route::get('install/', 'InstallerController@index')->name('install');
- 
+    Route::get('install/', 'InstallerController@index')->name('install'); 
 
-   // Route::get('check-environment', 'InstallerController@checkEnvironment')->name('check_environment');
-   // Route::post('check-environment', 'InstallerController@checkEnvironmentPost')->name('check_environment');
-
-   // Route::get('purchase-code-database', 'InstallerController@purchaseCodeVerification')->name('purchase_code_database');
-   // Route::post('purchase-code-database', 'InstallerController@purchaseCodeVerificationPost')->name('purchase_code_database');
-
-   // Route::get('system-setup-info', 'InstallerController@systemSetupInfo')->name('system-setup-info');
-
-   // Route::post('confirm-installing', 'InstallerController@confirmInstalling')->name('confirm-installing');
 });
 
 Route::group(['middleware' => ['XSS']], function () {
