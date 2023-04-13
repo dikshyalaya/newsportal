2345c221ba94380e8041a73f8729db2716e35117
Alien Shrestha <npalien4u@live.com>
2023-04-13 19:12:50 +0530

    create new post page layout updated

---
+++ Modules/Post/Resources/views/post_category_render_option.blade.php
@@ -0,0 +1,14 @@
+
+ 
+ <ul class="post_categories_select_list {{ @$depth>0?'child-list':'' }}">
+     @foreach ($categories as $category)
+
+     <li>
+         <input name="categories_id[]" type="checkbox" value="{{ $category->id }}" {{ in_array($category->id, $selected_categories)?'checked="checked"':'' }}  > {{ $category->category_name }}
+
+         @if($category->children!=null)
+         @include('post::post_category_render_option', ['categories'=>$category->children, 'depth'=>($depth+1), 'selected_categories'=>$selected_categories])
+         @endif
+     </li>
+     @endforeach
+ </ul>
