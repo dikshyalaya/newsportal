a3acb84a855ce2355ce6e63346f59ff8ae13538c
Alien Shrestha <npalien4u@live.com>
2023-04-16 14:56:58 +0530

    - save category and sub category function updated

--- Modules/Post/Http/Controllers/CategoryController.php
+++ Modules/Post/Http/Controllers/CategoryController.php
@@ -16,10 +16,11 @@ class CategoryController extends Controller
 {
     public function categories()
     {
-        $categories     = Category::orderBy('id', 'ASC')->paginate(10);
+        $categories       = Category::with('childrenRecursive')->where('parent_id',0)->paginate(10);
         $activeLang     = Language::where('status', 'active')->orderBy('name', 'ASC')->get();
+        $category=new Category();
 
-        return view('post::categories', compact('activeLang', 'categories'));
+        return view('post::edit_category', compact('activeLang', 'categories', 'category'));
     }
 
     public function saveNewCategory(Request $request)
@@ -34,7 +35,7 @@ class CategoryController extends Controller
         ])->validate();
 
         $category                   = new Category();
-
+        $category->parent_id        = $request->parent_id;
         $category->category_name    = $request->category_name;
         $category->language         = $request->language;
         $category->is_featured      = $request->is_featured;
@@ -60,10 +61,12 @@ class CategoryController extends Controller
 
     public function editCategory($id)
     {
+
         $category       = Category::find($id);
+        $categories       = Category::with('childrenRecursive')->where('parent_id',0)->paginate(10);
         $activeLang     = Language::where('status', 'active')->orderBy('name', 'ASC')->get();
 
-        return view('post::edit_category', compact('category', 'activeLang'));
+        return view('post::edit_category', compact('category','categories', 'activeLang'));
     }
 
     public function updateCategory(Request $request)
@@ -77,8 +80,13 @@ class CategoryController extends Controller
             'language'          => 'required'
         ])->validate();
 
-        $category                   = Category::find($request->category_id);
-
+        $category='';
+        if($request->category_id)
+            $category = Category::find($request->category_id);
+        else 
+            $category = new Category();
+            
+        $category->parent_id        = $request->parent_id;
         $category->category_name    = $request->category_name;
         $category->language         = $request->language;
         $category->is_featured      = $request->is_featured;
@@ -102,93 +110,7 @@ class CategoryController extends Controller
         return redirect()->route('categories')->with('success', __('successfully_updated'));
     }
 
-    public function subCategories()
-    {
-        $subCategories          = SubCategory::with('category')->orderBy('id', 'ASC')->paginate(10);
-        $categories             = Category::where('language', \App::getLocale() ?? settingHelper('default_language'))->get();
-        $activeLang             = Language::where('status', 'active')->orderBy('name', 'ASC')->get();
-
-        return view('post::sub_categories', compact('subCategories', 'activeLang', 'categories'));
-    }
-
-    public function subCategoriesAdd(Request $request)
-    {
-        if (strtolower(\Config::get('app.demo_mode')) == 'yes'):
-            return redirect()->back()->with('error', __('You are not allowed to add/modify in demo mode.'));
-        endif;
-        Validator::make($request->all(), [
-            'sub_category_name' => 'required|unique:sub_categories|min:2|max:40',
-            'language'          => 'required',
-            'slug'              => 'nullable|min:2|unique:categories|max:30|regex:/^\S*$/u',
-            'category_id'       => 'required'
-        ])->validate();
-
-        $subCategory                    = new SubCategory();
-
-        $subCategory->sub_category_name = $request->sub_category_name;
-        $subCategory->language          = $request->language;
-
-        if ($request->slug != null) :
-            $subCategory->slug  = $request->slug;
-        else :
-            $subCategory->slug  = $this->make_slug($request->sub_category_name);
-        endif;
-
-        $subCategory->meta_description  = $request->meta_description;
-        $subCategory->meta_keywords     = $request->meta_keywords;
-       // $subCategory->show_on_menu      = $request->show_on_menu;
-        $subCategory->category_id       = $request->category_id;
-
-        $subCategory->save();
-
-        Cache::Flush();
-
-        return redirect()->back()->with('success', __('successfully_added'));
-    }
-
-    public function editSubCategory($id)
-    {
-        $subCategory    = SubCategory::find($id);
-        $categories     = Category::all()->where('language', \App::getLocale() ?? settingHelper('default_language'));
-        $activeLang     = Language::where('status', 'active')->orderBy('name', 'ASC')->get();
-
-        return view('post::edit_sub_category', compact('subCategory', 'activeLang', 'categories'));
-    }
-
-    public function updateSubCategory(Request $request)
-    {
-        if (strtolower(\Config::get('app.demo_mode')) == 'yes'):
-            return redirect()->back()->with('error', __('You are not allowed to add/modify in demo mode.'));
-        endif;
-        Validator::make($request->all(), [
-            'sub_category_name'     => 'required|min:2|max:40|unique:sub_categories,sub_category_name,' . $request->sub_category_id,
-            'language'              => 'required',
-            'slug'                  => 'nullable|min:2|max:30|regex:/^\S*$/u|unique:sub_categories,slug,' . $request->sub_category_id,
-            'category_id'           => 'required'
-        ])->validate();
-
-        $subCategory                    = SubCategory::find($request->sub_category_id);
-        $subCategory->sub_category_name = $request->sub_category_name;
-        $subCategory->language          = $request->language;
-
-        if ($request->slug != null) :
-            $subCategory->slug  = $request->slug;
-        else :
-            $subCategory->slug  = $this->make_slug($request->sub_category_name);
-        endif;
-
-        $subCategory->meta_description  = $request->meta_description;
-        $subCategory->meta_keywords     = $request->meta_keywords;
-     //   $subCategory->show_on_menu      = $request->show_on_menu;
-        $subCategory->category_id       = $request->category_id;
-
-        $subCategory->save();
-
-        Cache::Flush();
-
-        return redirect()->route('sub-categories')->with('success', __('successfully_added'));
-    }
-
+   
     private function make_slug($string, $delimiter = '-') {
 
         $string = preg_replace("/[~`{}.'\"\!\@\#\$\%\^\&\*\(\)\_\=\+\/\?\>\<\,\[\]\:\;\|\\\]/", "", $string);
